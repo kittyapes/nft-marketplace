ea52affc9e4a5af10cbdbdf76d53b83f9b33658d
kittyapes <kittyapes@gmail.com>
2021-09-15 10:41:07 +1000

    batch mint & combine

--- contracts/PIXCluster.sol
+++ contracts/PIXCluster.sol
@@ -4,49 +4,77 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract PIXCluster is ERC721Enumerable, Ownable {
-    using Strings for uint256;
-
     mapping(address => bool) public moderators;
     mapping(uint8 => uint256) public prices;
 
-    mapping(uint256 => string) private _tokenURIs;
+    uint8 public constant MAX_PURCHASE = 20;
     string private _baseURIExtended;
 
-    modifier hasMintRole() {
-        require(
-            msg.sender == owner() || moderators[msg.sender],
-            "Sender is not owner or moderator"
-        );
+    modifier onlyMod() {
+        require(moderators[msg.sender], "Caller is not moderator");
         _;
     }
 
     constructor() ERC721("PIX Cluster", "PIX") {
-        console.log("Cluster NFT deployed");
+        console.log("PIX Cluster NFT deployed");
     }
 
-    function safeMint(address to)
-        external
-        hasMintRole
-        returns (uint256 tokenId)
-    {
+    function safeMint(address to) external onlyMod returns (uint256 tokenId) {
         tokenId = totalSupply();
         _safeMint(to, tokenId);
     }
 
-    function mint(uint8 category) external payable {
+    function safeBurn(uint256 tokenId) external {
         require(
-            msg.value >= prices[category],
-            "Insufficient for this category"
+            _isApprovedOrOwner(msg.sender, tokenId),
+            "Caller is not owner or operator"
         );
-        _safeMint(msg.sender, totalSupply());
+        _burn(tokenId);
+    }
+
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function setModerator(address moderator, bool approved) external onlyOwner {
+        require(moderator != address(0), "Moderator cannot be zero address");
+        moderators[moderator] = approved;
     }
 
     function setPrice(uint8 category, uint256 newPrice) external onlyOwner {
-        require(newPrice > 0, "Price must be non-zero value");
+        require(newPrice > 0, "Price cannot be zero");
         prices[category] = newPrice;
+    }
+
+    function mint(uint8[] calldata categories) external payable {
+        require(
+            categories.length <= MAX_PURCHASE,
+            "You cannot mint more than limit"
+        );
+
+        uint256 price;
+        for (uint256 i = 0; i < categories.length; i++) {
+            price += prices[categories[i]];
+        }
+        require(msg.value >= price, "Insufficient for purchase");
+
+        for (uint256 i = 0; i < categories.length; i++) {
+            _safeMint(msg.sender, totalSupply());
+        }
+    }
+
+    function combine(uint256 tokenId1, uint256 tokenId2) external {
+        address tokenOwner1 = ownerOf(tokenId1);
+        address tokenOwner2 = ownerOf(tokenId2);
+        require(
+            tokenOwner1 == msg.sender && tokenOwner2 == msg.sender,
+            "Caller is not owner of tokens"
+        );
+        _burn(tokenId1);
+        _burn(tokenId2);
+        _safeMint(msg.sender, totalSupply());
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
@@ -55,26 +83,5 @@ contract PIXCluster is ERC721Enumerable, Ownable {
 
     function setBaseURI(string memory baseURI_) external onlyOwner {
         _baseURIExtended = baseURI_;
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        returns (string memory)
-    {
-        require(_exists(tokenId), "TokenId is not existing");
-
-        string memory _tokenURI = _tokenURIs[tokenId];
-        string memory base = _baseURI();
-
-        if (bytes(base).length == 0) {
-            return _tokenURI;
-        }
-        if (bytes(_tokenURI).length > 0) {
-            return string(abi.encodePacked(base, _tokenURI));
-        }
-        return string(abi.encodePacked(base, tokenId.toString()));
     }
 }
