b1d346687730f01e8118da65cb8dd57b536954d2
kittyapes <kittyapes@gmail.com>
2021-11-21 15:02:44 +0900

    update deployment script

--- scripts/1 - cluster.js
+++ scripts/1 - cluster.js
@@ -1,12 +1,14 @@
 const { ethers, upgrades } = require('hardhat');
 
+const usdc = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174';
+
 async function main() {
   const PIXT = await ethers.getContractFactory('PIXT');
   const pixt = await PIXT.deploy();
   await pixt.deployed();
 
   const PIX = await ethers.getContractFactory('PIX');
-  const pix = await upgrades.deployProxy(PIX, [pixt.address]);
+  const pix = await upgrades.deployProxy(PIX, [pixt.address, usdc]);
   await pix.deployed();
 
   console.log('PIX NFT at ', pix.address);
