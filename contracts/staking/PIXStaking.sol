acb4882c9c0fb286e74642e3a2e7ec6a386f5419
kittyapes <kittyapes@gmail.com>
2022-03-21 13:11:35 +1000

    updated staking algorithm and completed unit test

--- contracts/staking/PIXStaking.sol
+++ contracts/staking/PIXStaking.sol
@@ -9,6 +9,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol"
 import "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
 import "../interfaces/IPIX.sol";
 
 contract PIXStaking is OwnableUpgradeable, ReentrancyGuardUpgradeable, ERC721HolderUpgradeable {
@@ -18,6 +19,7 @@ contract PIXStaking is OwnableUpgradeable, ReentrancyGuardUpgradeable, ERC721Hol
     event StakedPixNFT(uint256 tokenId, address indexed recipient);
     event WithdrawnPixNFT(uint256 tokenId, address indexed recipient);
     event ClaimPixNFT(uint256 pending, address indexed recipient);
+    event RewardAdded(uint256 reward);
 
     struct UserInfo {
         mapping(uint256 => bool) isStaked;
@@ -31,39 +33,81 @@ contract PIXStaking is OwnableUpgradeable, ReentrancyGuardUpgradeable, ERC721Hol
     IERC20Upgradeable public rewardToken;
 
     address public pixNFT;
-    uint256 public lastUpdateBlock;
-    uint256 public rewardPerBlock;
     uint256 public totalTiers;
-    uint256 public accPixNFTPerShare;
-    uint256 constant ACC_PIX_PRECISION = 1e12;
 
-    modifier updateRewardPool() {
-        if (totalTiers > 0) {
-            uint256 reward = _calculateReward();
-            accPixNFTPerShare = accPixNFTPerShare.add(
-                reward.mul(ACC_PIX_PRECISION).div(totalTiers)
-            );
+    uint256 public constant DURATION = 10 days;
+    uint256 public periodFinish;
+    uint256 public rewardRate;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTierStored;
+    address public rewardDistributor;
+    mapping(address => uint256) public userRewardPerTierPaid;
+    mapping(address => uint256) public rewards;
+
+    modifier updateReward(address account) {
+        rewardPerTierStored = rewardPerTier();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTierPaid[account] = rewardPerTierStored;
         }
-        lastUpdateBlock = block.number;
         _;
     }
 
-    function initialize(
-        address _pixt,
-        address _pixNFT,
-        uint256 _rewardPerBlock
-    ) external initializer {
+    modifier onlyRewardDistributor() {
+        require(msg.sender == rewardDistributor, "Staking: NON_DISTRIBUTOR");
+        _;
+    }
+
+    function initialize(address _pixt, address _pixNFT) external initializer {
         require(_pixt != address(0), "Staking: INVALID_PIXT");
         require(_pixNFT != address(0), "Staking: INVALID_PIX");
         rewardToken = IERC20Upgradeable(_pixt);
         pixNFT = _pixNFT;
-        rewardPerBlock = _rewardPerBlock;
         __Ownable_init();
         __ReentrancyGuard_init();
         __ERC721Holder_init();
     }
 
-    function stake(uint256 _tokenId) external updateRewardPool {
+    /// @dev validation reward period
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return MathUpgradeable.min(block.timestamp, periodFinish);
+    }
+
+    /// @dev reward rate per staked token
+    function rewardPerTier() public view returns (uint256) {
+        if (totalTiers == 0) {
+            return rewardPerTierStored;
+        }
+        return
+            rewardPerTierStored +
+            ((lastTimeRewardApplicable() - lastUpdateTime) * rewardRate * 1e18) /
+            totalTiers;
+    }
+
+    /**
+     * @dev view total stacked reward for user
+     * @param account target user address
+     */
+    function earned(address account) public view returns (uint256) {
+        return
+            (userInfo[account].tiers * (rewardPerTier() - userRewardPerTierPaid[account])) /
+            1e18 +
+            rewards[account];
+    }
+
+    /**
+     * @dev set reward distributor by owner
+     * reward distributor is the moderator who calls {notifyRewardAmount} function
+     * whenever periodic reward tokens transferred to this contract
+     * @param distributor new distributor address
+     */
+    function setRewardDistributor(address distributor) external onlyOwner {
+        require(distributor != address(0), "Staking: INVALID_DISTRIBUTOR");
+        rewardDistributor = distributor;
+    }
+
+    function stake(uint256 _tokenId) external updateReward(msg.sender) {
         require(_tokenId > 0, "Staking: INVALID_TOKEN_ID");
         require(tierInfo[_tokenId] > 0, "Staking: INVALID_TIER");
         require(IPIX(pixNFT).isTerritory(_tokenId), "Staking: TERRITORY_ONLY");
@@ -72,71 +116,68 @@ contract PIXStaking is OwnableUpgradeable, ReentrancyGuardUpgradeable, ERC721Hol
 
         uint256 tiers = tierInfo[_tokenId];
 
-        if (user.tiers > 0) {
-            uint256 pending = user.tiers.mul(accPixNFTPerShare).div(ACC_PIX_PRECISION).sub(
-                user.rewardDebt
-            );
-            rewardToken.safeTransfer(msg.sender, pending);
-        }
-
         IERC721Upgradeable(pixNFT).safeTransferFrom(msg.sender, address(this), _tokenId);
         totalTiers = totalTiers.add(tiers);
 
         // Update User Info
         user.tiers = user.tiers.add(tiers);
-        user.rewardDebt = user.tiers.mul(accPixNFTPerShare).div(ACC_PIX_PRECISION);
         user.isStaked[_tokenId] = true;
 
         emit StakedPixNFT(_tokenId, address(this));
     }
 
-    function withdraw(uint256 _tokenId) external updateRewardPool nonReentrant {
+    function withdraw(uint256 _tokenId) external updateReward(msg.sender) nonReentrant {
         require(_tokenId > 0, "Staking: INVALID_TOKEN_ID");
         UserInfo storage user = userInfo[msg.sender];
         require(user.tiers > 0, "Staking: NO_WITHDRAWALS");
         require(user.isStaked[_tokenId], "Staking: NO_STAKES");
 
-        uint256 pending = user.tiers.mul(accPixNFTPerShare).div(ACC_PIX_PRECISION).sub(
-            user.rewardDebt
-        );
-        rewardToken.safeTransfer(msg.sender, pending);
-
         IERC721Upgradeable(pixNFT).safeTransferFrom(address(this), msg.sender, _tokenId);
         totalTiers = totalTiers.sub(tierInfo[_tokenId]);
         // Update UserInfo
         user.tiers = user.tiers.sub(tierInfo[_tokenId]);
-        user.rewardDebt = user.tiers.mul(accPixNFTPerShare).div(ACC_PIX_PRECISION);
         user.isStaked[_tokenId] = false;
 
         emit WithdrawnPixNFT(_tokenId, msg.sender);
     }
 
-    function claim() external updateRewardPool {
-        UserInfo storage user = userInfo[msg.sender];
-        require(user.tiers > 0, "Staking: NO_WITHDRAWALS");
-
-        uint256 pending = user.tiers.mul(accPixNFTPerShare).div(ACC_PIX_PRECISION).sub(
-            user.rewardDebt
-        );
-        if (pending > 0) {
-            rewardToken.safeTransfer(msg.sender, pending);
-            emit ClaimPixNFT(pending, msg.sender);
+    /**
+     * @dev claim reward and update reward related arguments
+     * @notice emit {RewardPaid} event
+     */
+    function claim() public updateReward(msg.sender) {
+        uint256 reward = earned(msg.sender);
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            rewardToken.safeTransfer(msg.sender, reward);
+            emit ClaimPixNFT(reward, msg.sender);
         }
-        // Update UserInfo
-        user.rewardDebt = user.tiers.mul(accPixNFTPerShare).div(ACC_PIX_PRECISION);
     }
 
-    function setRewardPerBlock(uint256 _amount) external onlyOwner {
-        rewardPerBlock = _amount;
+    /**
+     * @dev update reward related arguments after reward token arrived
+     * @param reward reward token amounts received
+     * @notice emit {RewardAdded} event
+     */
+    function notifyRewardAmount(uint256 reward)
+        external
+        onlyRewardDistributor
+        updateReward(address(0))
+    {
+        if (block.timestamp >= periodFinish) {
+            rewardRate = reward / DURATION;
+        } else {
+            uint256 remaining = periodFinish - block.timestamp;
+            uint256 leftover = remaining * rewardRate;
+            rewardRate = (reward + leftover) / DURATION;
+        }
+        lastUpdateTime = block.timestamp;
+        periodFinish = block.timestamp + DURATION;
+        emit RewardAdded(reward);
     }
 
     function setTierInfo(uint256 _tokenId, uint256 _tier) external onlyOwner {
         require(_tier > 0, "Staking: INVALID_TIERS");
         tierInfo[_tokenId] = _tier;
-    }
-
-    function _calculateReward() internal view returns (uint256) {
-        uint256 blocksPassed = block.number.sub(lastUpdateBlock);
-        return rewardPerBlock.mul(blocksPassed);
     }
 }
--- test/PIXStaking.test.ts
+++ test/PIXStaking.test.ts
@@ -1,9 +1,8 @@
 import { expect } from 'chai';
 import { ethers, upgrades } from 'hardhat';
-import { Signer, Contract, BigNumber } from 'ethers';
-import { PIXCategory, PIXSize } from './utils';
+import { Signer, Contract, BigNumber, constants } from 'ethers';
+import { PIXCategory, PIXSize, increaseTime } from './utils';
 import { time } from '@openzeppelin/test-helpers';
-
 describe('PIXStaking', function () {
   let owner: Signer;
   let alice: Signer;
@@ -28,30 +27,34 @@ describe('PIXStaking', function () {
     pixNFT = await upgrades.deployProxy(PIXFactory, [pixToken.address, usdc.address]);
 
     const PIXStakingFactory = await ethers.getContractFactory('PIXStaking');
-    pixStaking = await upgrades.deployProxy(PIXStakingFactory, [
-      pixToken.address,
-      pixNFT.address,
-      rewardPerBlock,
-    ]);
+    pixStaking = await upgrades.deployProxy(PIXStakingFactory, [pixToken.address, pixNFT.address]);
 
     await pixNFT.setTrader(pixStaking.address, true);
     await pixNFT.safeMint(await alice.getAddress(), [0, PIXCategory.Common, PIXSize.Area]);
     await pixToken.transfer(pixStaking.address, ethers.utils.parseEther('1000000'));
     await pixToken.transfer(await alice.getAddress(), BigNumber.from(10000));
     await pixToken.transfer(await bob.getAddress(), BigNumber.from(10000));
+
+    await pixStaking.connect(owner).setRewardDistributor(await owner.getAddress());
+    await pixStaking.connect(owner).notifyRewardAmount(BigNumber.from(864000));
   });
 
-  describe('setRewardPerBlock', () => {
+  describe('setRewardDistributor', () => {
     it('it should set reward amount correctly', async () => {
-      await pixStaking.connect(owner).setRewardPerBlock(ethers.utils.parseEther('2.0'));
-      expect(await pixStaking.rewardPerBlock()).to.equal(ethers.utils.parseEther('2.0'));
+      await pixStaking.connect(owner).setRewardDistributor(await alice.getAddress());
+      expect(await pixStaking.rewardDistributor()).to.equal(await alice.getAddress());
     });
-  });
 
-  describe('setTierInfo', () => {
-    it('it should set tier amount correctly', async () => {
-      await pixStaking.setTierInfo('1', '2');
-      expect(await pixStaking.tierInfo(1)).to.equal('2');
+    it('it should revert if the caller is not a owner', async () => {
+      await expect(
+        pixStaking.connect(alice).setRewardDistributor(await bob.getAddress()),
+      ).to.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('it should revert if the distributor address is zero address', async () => {
+      await expect(
+        pixStaking.connect(owner).setRewardDistributor(constants.AddressZero),
+      ).to.revertedWith('Staking: INVALID_DISTRIBUTOR');
     });
   });
 
@@ -81,24 +84,24 @@ describe('PIXStaking', function () {
       await pixNFT.connect(alice).approve(pixStaking.address, 1);
       await pixStaking.connect(alice).stake(1);
 
-      await time.advanceBlock();
-      await time.advanceBlock();
-      await time.advanceBlock();
-      await time.advanceBlock();
-      await time.advanceBlock();
+      await increaseTime(BigNumber.from(50));
+    });
+
+    it('should return correct rewards amount', async function () {
+      expect(await pixStaking.earned(await alice.getAddress())).to.closeTo(
+        BigNumber.from(50),
+        1,
+        '',
+      );
     });
 
     it('should provide correct rewards', async function () {
       await pixStaking.connect(alice).claim();
       expect(await pixToken.balanceOf(await alice.getAddress())).to.closeTo(
         BigNumber.from(10050),
-        10,
+        1,
         '',
       );
-    });
-
-    it('should revert if didnt stake', async function () {
-      await expect(pixStaking.connect(bob).claim()).to.revertedWith('Staking: NO_WITHDRAWALS');
     });
   });
 
@@ -108,19 +111,13 @@ describe('PIXStaking', function () {
       await pixStaking.setTierInfo('1', '2');
       await pixNFT.connect(alice).approve(pixStaking.address, 1);
       await pixStaking.connect(alice).stake(1);
-
-      await time.advanceBlock();
-      await time.advanceBlock();
-      await time.advanceBlock();
-      await time.advanceBlock();
-      await time.advanceBlock();
     });
 
     it('should provide correct rewards', async function () {
       await pixStaking.connect(alice).withdraw(1);
       expect(await pixToken.balanceOf(await alice.getAddress())).to.closeTo(
-        BigNumber.from(10050),
-        10,
+        BigNumber.from(10000),
+        1,
         '',
       );
     });
