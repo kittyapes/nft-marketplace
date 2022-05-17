a97f59cd7bf438cab02ae4a036aa3fe29c1df719
kittyapes <kittyapes@gmail.com>
2022-05-18 01:49:47 +1000

    batch transfer + moderator on pool together

--- scripts/impl.js
+++ scripts/impl.js
@@ -1,12 +1,8 @@
 const { ethers, upgrades } = require('hardhat');
 
 async function main() {
-  const ContractFactory = await ethers.getContractFactory('PIXTStakingLottery');
-  const contract = await upgrades.deployProxy(ContractFactory, [
-    '0xae5039fc6D8360008419E169d54F1C81c665c55D',
-    10,
-    86400 * 7,
-  ]);
+  const ContractFactory = await ethers.getContractFactory('PIX');
+  const contract = await ContractFactory.deploy();
   await contract.deployed();
 
   console.log('Deployed at', contract.address);
